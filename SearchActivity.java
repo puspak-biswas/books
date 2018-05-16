54ac7555cd95dde0675db035be3b5acc9830be8a
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-05-16 23:26:47 +0530

    Create SearchActivity.java

---
+++ SearchActivity.java
@@ -0,0 +1,23 @@
+package com.example.puspakbiswas.books;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void onSearch(View view){
+        EditText keywordText = (EditText) findViewById(R.id.keyword);
+        String keyword = keywordText.getText().toString();
+        String url = "https://www.googleapis.com/books/v1/volumes?q="+keyword;
+        Log.i("URL:",url);
+    }
+}
