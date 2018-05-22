ef186afe2d1256601a497bcd5b0c9ce74a6623f4
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-05-22 08:53:43 +0530

    Update DisplayActivity.java

--- DisplayActivity.java
+++ DisplayActivity.java
@@ -2,6 +2,11 @@ package com.example.puspakbiswas.books;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        displayMethod();
+    }
+
+    public void displayMethod(){
+        ArrayList<books> bookList = new ArrayList<books>();
+        bookList.add(new books("The idea of justice"));
+        bookList.add(new books("The argumentative Indian"));
+
+        ArrayAdapter<books> booksAdapter = new ArrayAdapter<books>(this,0,bookList);
+        ListView booksView = (ListView) findViewById(R.id.list);
+        booksView.setAdapter(booksAdapter);
     }
 }
+
