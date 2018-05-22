fb5cbf43ac012d80294151d55a0096357d2087c6
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-05-22 09:00:33 +0530

    Create books.java

---
+++ books.java
@@ -0,0 +1,18 @@
+package com.example.puspakbiswas.books;
+
+/**
+ * Created by Puspak Biswas on 19-05-2018.
+ */
+
+public class books {
+
+    private String mTitle;
+
+    public books(String t){
+        mTitle = t;
+    }
+
+    public String getTitle(){
+        return mTitle;
+    }
+}
