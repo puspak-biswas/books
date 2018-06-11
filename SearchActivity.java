e9b3772f648ecc200a6fa89ceff74c9ea2caa501
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-06-11 11:08:11 +0530

    Update SearchActivity.java

--- SearchActivity.java
+++ SearchActivity.java
@@ -2,6 +2,7 @@ package com.example.puspakbiswas.books;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,10 +21,24 @@ public class SearchActivity extends AppCompatActivity {
        EditText keywordText = (EditText) findViewById(R.id.keyword);
        String keyword = keywordText.getText().toString();
        String url = "https://www.googleapis.com/books/v1/volumes?q="+keyword;
-       QueryUtils.fetchBookData(url);
+        Log.i("URL:",url);
+        ExperimentClass task = new ExperimentClass();
+        task.execute(url);
        // Log.i("URL:",url);
-       // Intent intent = new Intent(this,DisplayActivity.class);
+        //Intent intent = new Intent(this,DisplayActivity.class);
        // intent.setData(Uri.parse(url));
-       // startActivity(intent);
+        //startActivity(intent);
+    }
+
+    class ExperimentClass extends AsyncTask<String,Void,String>{
+
+        protected String doInBackground(String... urls){
+            QueryUtils.fetchBookData(urls[0]);
+            String dum = "dummy";
+            return dum;
+        }
+        protected void onPostExecute(String dum){
+            Log.i("Dummy",dum);
+        }
     }
 }
