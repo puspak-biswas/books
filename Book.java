030d60dd61eb9e2c5acd55e9f08dba34b1e22788
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-06-14 00:57:01 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,29 +1,40 @@
 package com.example.puspakbiswas.books;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 /**
- * Created by Puspak Biswas on 19-05-2018.
+ * Created by Puspak Biswas on 27-05-2018.
  */
 
-public class Book {
+public class bookAdapter extends ArrayAdapter<Book> {
 
-    private String mTitle;
-    private ArrayList<String> mAuthor;
-
-    public Book(String t, ArrayList<String> a){
-
-        mTitle = t;
-        mAuthor = a;
+    public bookAdapter(Context c, ArrayList<Book> bookList){
+        super(c,0,bookList);
     }
 
-    public String getTitle(){
-        return mTitle;
-    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        View listBookView = convertView;
+        if (listBookView == null){
+            listBookView = LayoutInflater.from(getContext()).inflate(R.layout.list,parent,false);
+        }
 
-    public ArrayList<String> getAuthor(){return mAuthor;}
+        Book book = getItem(position);
 
-    public String toString(){
-        return mTitle;
+        TextView title = (TextView) listBookView.findViewById(R.id.title);
+        title.setText(book.getTitle());
+
+        TextView author = (TextView) listBookView.findViewById(R.id.author);
+        //author.setText(book.getAuthor());
+
+        return listBookView;
     }
 }
