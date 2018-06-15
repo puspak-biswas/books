24e960793b3d4fd21bc1b2cc4a39dfd28ee88817
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-06-15 11:56:57 +0530

    Update DisplayActivity.java

--- DisplayActivity.java
+++ DisplayActivity.java
@@ -14,10 +14,13 @@
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_display);
-            displayMethod();
+            Intent intent = getIntent();
+            Bundle args = intent.getBundleExtra("BUNDLE");
+            ArrayList<Book> bookList = (ArrayList<Book>) args.getSerializable("ARRAYLIST");
+            displayMethod(bookList);
         }
 
-        public void displayMethod(){
+        public void displayMethod(ArrayList<Book> bookList){
             ArrayList<Book> bookList = new ArrayList<Book>();
             //bookList.add(new Book("The idea of justice","Amartya Sen"));
             //bookList.add(new Book("The argumentative Indian","Amartya Sen"));
