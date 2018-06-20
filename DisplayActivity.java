4b7b5ac91ee64fc80ab2ca79a3c395dda1b4b635
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-06-20 08:33:14 +0530

    Update DisplayActivity.java

--- DisplayActivity.java
+++ DisplayActivity.java
@@ -1,6 +1,10 @@
     package com.example.puspakbiswas.books;
 
+    import android.app.LoaderManager;
     import android.content.Intent;
+    import android.content.Loader;
+    import android.os.AsyncTask;
+    import android.app.LoaderManager.LoaderCallbacks;
     import android.support.v7.app.AppCompatActivity;
     import android.os.Bundle;
     import android.widget.ArrayAdapter;
@@ -9,55 +13,40 @@
 
     import java.util.ArrayList;
 
-    public class DisplayActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>>{
+    public class DisplayActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Book>> {
+
+        private String URL;
+        private bookAdapter mAdapter;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_display);
             Intent intent = getIntent();
-            String url = intent.getExtras.getString("StringUrl");
-            LoaderManager loaderManager = getLoaderManager();
-            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-            //ExperimentClass task = new ExperimentClass();
-            //task.execute(url);
-        }
-        
-        public Loader<ArrayList<Book>> onCreateLoader(int i, Bundle bundle) {
-        // TODO: Create a new loader for the given URL
-        Loader<ArrayList<Book>> loader = new BookLoader(this,queryString);
-        return loader;
-    }
-        
-        public void onLoadFinished(Loader<ArrayList<Book>> loader, ArrayList<Book> bookList) {
-        if (bookList != null && !bookList.isEmpty()) {
-            displayMethod(bookList);
-        }
-        // TODO: Update the UI with the result
-    }
-        
-        public void onLoaderReset(Loader<ArrayList<Book>> loader) {
-        bookAdapter.clear();
-        // TODO: Loader reset, so we can clear out our existing data.
-    }
-
-        public void displayMethod(ArrayList<Book> bookList){
-            //ArrayList<Book> bookList = new ArrayList<Book>();
-            //bookList.add(new Book("The idea of justice","Amartya Sen"));
-            //bookList.add(new Book("The argumentative Indian","Amartya Sen"));
-
-            bookAdapter booksAdapter = new bookAdapter(this,bookList);
+            URL = intent.getExtras().getString("StringUrl");
+            mAdapter = new bookAdapter(this,new ArrayList<Book>());
             ListView booksView = (ListView) findViewById(R.id.list);
-            booksView.setAdapter(booksAdapter);
+            booksView.setAdapter(mAdapter);
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(0, null, this);
         }
-        //class ExperimentClass extends AsyncTask<String,Void,ArrayList<Book>>{
 
-        //protected ArrayList<Book> doInBackground(String... urls){
-        //    ArrayList<Book> bookList = QueryUtils.fetchBookData(urls[0]);
-        //    return bookList;
-        //}
-        //protected void onPostExecute(ArrayList<Book> bookList){
-        //    displayMethod(bookList);
-        //}
-    //}
+        public Loader<ArrayList<Book>> onCreateLoader(int i, Bundle bundle){
+            Loader<ArrayList<Book>> loader = new BookLoader(this,URL);
+            return loader;
+        }
+
+        public void onLoadFinished(Loader<ArrayList<Book>> loader,ArrayList<Book> bookList){
+            if (bookList != null && !bookList.isEmpty()) {
+                mAdapter.addAll(bookList);
+            }
+        }
+
+        public void onLoaderReset(Loader<ArrayList<Book>> loader){
+            mAdapter.clear();
+        }
+
+
+
+    }
 
