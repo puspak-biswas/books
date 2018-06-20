c7fa4b2d4b03b0bad14bfd58013abc2955a3b45d
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-06-20 08:33:34 +0530

    Update BookLoader.java

--- BookLoader.java
+++ BookLoader.java
@@ -1,35 +1,34 @@
+package com.example.puspakbiswas.books;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
- * Created by Puspak Biswas on 10-05-2018.
+ * Created by Puspak Biswas on 19-06-2018.
  */
 
 public class BookLoader extends AsyncTaskLoader<ArrayList<Book>> {
-    private String URL;
 
-    public BookLoader(Context context, String url) {
+    private String mURL;
+    public BookLoader(Context context, String url){
         super(context);
-        URL = url;
-        // TODO: Finish implementing this constructor
+        mURL = url;
     }
 
-    @Override
     protected void onStartLoading() {
         forceLoad();
     }
 
-    @Override
-    public ArrayList<Book> loadInBackground() {
-        ArrayList<Book> bookList = null;
-        if(URL != null) {
-            // Create a fake list of earthquake locations.
-            bookList = QueryUtils.fetchBookData(URL);
+    public ArrayList<Book> loadInBackground(){
+        if (mURL == null || mURL==""){
+            return null;
+        }else {
+            ArrayList<Book> bookList = QueryUtils.fetchBookData(mURL);
+            return bookList;
         }
-        return bookList;
-        // TODO: Implement this method
+
+
     }
 }
